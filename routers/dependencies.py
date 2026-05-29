5e8471ca2f71217ae39a5ebd7e4ddafcebd751f6
Alomyr <matheuscastro11213@gmail.com>
2026-05-29 10:07:26 -0300

    define unica funçao de de validação e retorno de objeto

--- routers/dependencies.py
+++ routers/dependencies.py
@@ -3,34 +3,6 @@ from fastapi import APIRouter, Depends, HTTPException
 from model.database import get_db
 from sqlalchemy.orm import Session
 
-
-def check_if_exists(db: Session, model, filter_name: str, data):
-
-    column = getattr(model, filter_name)
-    value = getattr(data, filter_name)
-    is_exists = db.query(model).filter(column == value).first() is not None
-    return is_exists
-
-
-# colocar funções de reutilizadas aqui
-
-
-def get_record(db: Session, model, filter_name: str, data):
-
-    column = getattr(model, filter_name)
-    value = getattr(data, filter_name)
-    is_exists = db.query(model).filter(column == value).first()
-    return is_exists
-
-
-# def get_record(db: Session, model, filter_name_user: str, filter_name_lead, data):
-
-#     column = getattr(model, filter_name_user)
-#     value = getattr(data, filter_name_lead)
-#     is_exists = db.query(model).filter(column == value).first()
-#     return is_exists
-
-
 def get_record(db: Session, model, filter_data: dict, retorn_obj: bool = False):
 
     for column_name, value in filter_data.items():
