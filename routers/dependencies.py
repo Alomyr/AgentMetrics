b90ccb9fd3daa7dcd60706454dc25456a29ba3f6
Alomyr <matheuscastro11213@gmail.com>
2026-05-29 09:32:49 -0300

    sobe inicio de refatoração dos metodos lead

--- routers/dependencies.py
+++ routers/dependencies.py
@@ -22,4 +22,14 @@ def check_if_exists_login(db: Session, model, filter_name: str, data):
     return is_exists
 
 
+def check_if_exists_lead_user_association(
+    db: Session, model, filter_name_user: str, filter_name_lead, data
+):
+
+    column = getattr(model, filter_name_user)
+    value = getattr(data, filter_name_lead)
+    is_exists = db.query(model).filter(column == value).first()
+    return is_exists
+
+
 # refatorar para ser uma função unica essas duas depois
